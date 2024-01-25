cd3befed1b06b1e57f465766e9b8c557ad1b460b
Anthony McLamb <adylanmclamb@yahoo.com>
2024-01-25 01:59:42 -0500

    Adding search on navbar

---
+++ src/features/weather/current-weather/CurrentWeather.tsx
@@ -0,0 +1,7 @@
+type Props = {};
+
+const CurrentWeather = (props: Props) => {
+  return <div>CurrentWeather</div>;
+};
+
+export default CurrentWeather;
--- src/layout/navbar/navbar-mobile/NavbarMobile.tsx
+++ src/layout/navbar/navbar-mobile/NavbarMobile.tsx
@@ -4,6 +4,7 @@ import {
   SheetHeader,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import WeatherSearch from "@/features/weather/weather-search/WeatherSearch";
 import { Menu } from "lucide-react";
 import { Link } from "react-router-dom";
 
@@ -21,12 +22,17 @@ const NavbarMobile = ({ className = "" }: Props) => {
         <SheetContent side="left" className="flex flex-col">
           <SheetHeader>
             <div className="flex justify-start">
-              <Link to="/" className="font-semibold">
-                Ai Adlibs
+              <Link to="/" className="flex items-center">
+                <img
+                  src="https://openweathermap.org/img/wn/02d.png"
+                  className="-ml-1"
+                />
+                <h1 className="text-lg font-semibold py-2">Weatherly</h1>
               </Link>
             </div>
           </SheetHeader>
           <div className="flex flex-col gap-5">
+            <WeatherSearch />
             <ul className="gap-5 flex flex-col">
               <li>
                 <Link to="/" className="py-2 pl-0 pr-2 block">
