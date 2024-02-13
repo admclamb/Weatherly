6d19ff8604e442faeab760da866276e00c9c79bf
Anthony McLamb <adylanmclamb@yahoo.com>
2024-02-12 22:47:59 -0500

    Remove testing

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -8,9 +8,7 @@ const Home = () => {
       <div data-testid="home">
         <Hero />
         <Container className="px-3 lg:px-0 py-5 grid-cols-9 grid gap-5">
-          <section className="col-span-9 lg:col-span-6 flex flex-col gap-5">
-            <h1>TESTING</h1>
-          </section>
+          <section className="col-span-9 lg:col-span-6 flex flex-col gap-5"></section>
           <aside className="col-span-3 "></aside>
         </Container>
       </div>
